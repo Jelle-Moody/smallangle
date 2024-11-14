3b69d2a2be3ac13e9bcedace16fac3714b564c18
Jelle-Moody <jelle.moody@student.uva.nl>
2024-11-14 11:37:33 +0100

    Code herschreven
    De vorige code werkte, maar was niet heel mooi, omdat het geen gebruik maakte van de subcommands. Nu doet de code dat wel en de code werkt weer.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,8 +3,11 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.command()
-@click.argument("function")
+@click.group()
+def smallangle():
+    pass
+
+@smallangle.command()
 @click.option(
     "-n",
     "--number",
@@ -12,19 +15,19 @@ import pandas as pd
     help="Number of steps between 0 and 2pi.",
     show_default=True,  # show default in help
 )
-def smallangle(function, number):
-    if function == "sin":
-        sin(number)
-    if function == "tan":
-        tan(number)
-    if function != "sin" and function!= "tan":
-        print("Function not recognized: try sin or tan")
-
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
+@smallangle.command()
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
@@ -32,4 +35,4 @@ def tan(number):
 
 
 if __name__ == "__main__":
-    sin(10)
+    smallangle()
