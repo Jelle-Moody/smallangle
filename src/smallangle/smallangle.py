33a9358a7271bcda5b9298f0d076b3741254ef6e
Jelle-Moody <jelle.moody@student.uva.nl>
2024-11-14 11:50:37 +0100

    Docstrings toegevoegd
    Ik heb docstrings toegevoegd aan de functies in smallangle.py.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 @click.group()
 def smallangle():
+    """Initializes the click group. Shows The help text when run.
+    """    
     pass
 
 @smallangle.command()
@@ -16,6 +18,11 @@ def smallangle():
     show_default=True,  # show default in help
 )
 def sin(number):
+    """Produces a list of points between 0 and 2pi and their sine.
+
+    Args:
+        number (int): number of steps between 0 and 2pi
+    """    
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
@@ -29,6 +36,11 @@ def sin(number):
     show_default=True,  # show default in help
 )
 def tan(number):
+    """Produces a list of points between 0 and 2pi and their tangent.
+
+    Args:
+        number (int): number of steps between 0 and 2pi
+    """ 
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
     print(df)
