c499010274d9fa59ac695711080d0d3c6931ad14
Jelle-Moody <jelle.moody@student.uva.nl>
2024-11-14 10:50:16 +0100

    Click command volledig gemaakt
    Click command van smallangle compleet gemaakt met als argument de functie: sin of tan, en als option het aantal stappen. De subcommands zijn nu niet meer nodig dus die zijn weggehaald en smallangle is geen group meer, maar gewoon een command.

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -3,19 +3,28 @@ import numpy as np
 from numpy import pi
 import pandas as pd
 
-@click.group()
-def smallangle():
-    pass
+@click.command()
+@click.argument("function")
+@click.option(
+    "-n",
+    "--number",
+    default=10,
+    help="Number of steps between 0 and 2pi.",
+    show_default=True,  # show default in help
+)
+def smallangle(function, number):
+    if function == "sin":
+        sin(number)
+    if function == "tan":
+        tan(number)
+    if function != "sin" and function!= "tan":
+        print("Function not recognized: try sin or tan")
 
-@smallangle.command()
-@click.argument()
 def sin(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "sin (x)": np.sin(x)})
     print(df)
 
-@smallangle.command()
-@click.argument()
 def tan(number):
     x = np.linspace(0, 2 * pi, number)
     df = pd.DataFrame({"x": x, "tan (x)": np.tan(x)})
